69f7b373a94945be08955e745f8989cb68d855b0
Date Huang <tjjh89017@hotmail.com>
2014-11-16 20:25:30 +0800

    fix

--- arch/x86/kernel/project.c
+++ arch/x86/kernel/project.c
@@ -46,7 +46,7 @@ asmlinkage int sys_project(long pid) {
 		/*
 		 * linear address from vm_start to vm_end
 		 */
-		len += snprintf(buf + len, BUF_SIZE - len, "%x-%x ", vm->vm_start, vm->vm_end);
+		len += snprintf(buf + len, BUF_SIZE - len, "%8x-%8x ", vm->vm_start, vm->vm_end);
 
 		/*
 		 * permission
